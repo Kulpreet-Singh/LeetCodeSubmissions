9eb767a54efb9818be98cb293a6e8a8ecb785209
Kulpreet Singh <43897531+Kulpreet-Singh@users.noreply.github.com>
2024-05-14 16:33:18 +0530

    Time: 9 ms (86.73%) | Memory: 20.3 MB (85.05%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,9 +1,35 @@
 class Solution {
 public:
+
+// 000 - 0
+// 001 - 1
+// 010 - 3
+// 011 - 0
+// 100 - 4
+// 101 - 1
+// 110 - 7
+// 111 - 0
+// 1000 - 8
+
+    int findXor1ToN(int n){
+        switch(n%4){
+            case 0: return n;
+            case 1: return 1;
+            case 2: return n+1;
+            case 3: return 0;
+            default: return -1;
+        }
+        return -1;
+    }
+
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        int expectedSum = (n*(n+1))/2;
-        int actualSum = accumulate(nums.begin(), nums.end(), 0);
-        return expectedSum - actualSum;
+        int expectedXor = findXor1ToN(n);
+        int actualXor = 0;
+        for(auto it: nums){
+            actualXor = actualXor ^ it;
+        }
+        return expectedXor ^ actualXor;
     }
 };
+
