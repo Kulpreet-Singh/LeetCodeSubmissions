34540362275f84dfe4a33563b8261fac45de40c2
Kulpreet Singh <43897531+Kulpreet-Singh@users.noreply.github.com>
2024-05-15 01:54:25 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (95.05%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
+//Dutch National flag algorithm
+    void swap(int &a, int &b){
+        int temp = a;
+        a = b;
+        b = temp;
+    }
     void sortColors(vector<int>& nums) {
-        int cnt0 =0, cnt1 = 0, cnt2 = 0;
-        for(auto it: nums){
-            if(it ==0) cnt0++;
-            else if (it ==1) cnt1++;
-            else if (it ==2) cnt2++;
-        }
-        int ind = 0;
-        for ( int i=0;i<cnt0; i++){
-            nums[ind+i] = 0;
-        }
-        ind = cnt0;
-        for ( int i=0;i<cnt1; i++){
-            nums[ind+i] = 1;
-        }
-        ind = cnt0+cnt1;
-        for ( int i=0;i<cnt2; i++){
-            nums[ind+i] = 2;
+        int low =0;
+        int mid =0;
+        int high = nums.size()-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            } else if (nums[mid]==1){
+                mid++;
+                // only incrementing mid here since 1 is expected in the middle only
+            } else if(nums[mid]==2){
+                swap(nums[mid], nums[high]);
+                high--;
+                // not incrementing mid here since low and mid can still be unsorted
+            }
         }
     }
 };
