9925c68c297c5e917809671027c238865dc30a1a
Kulpreet Singh <43897531+Kulpreet-Singh@users.noreply.github.com>
2024-05-16 04:28:12 +0530

    Time: 8 ms (88.19%) | Memory: 17.3 MB (36.04%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,3 +1,6 @@
+bool comparator(const pair<int, int>& a, const pair<int, int>& b) {
+    return a.second > b.second;
+}
 class Solution {
 public:
 // hashing + heap
@@ -7,16 +10,14 @@ public:
         for(auto num: nums){
             mpp[num]++;
         }
-        // max heap of the hashed values
-        priority_queue<pair<int, int>> pq;
-        for(auto &[key, value]: mpp){
-            pq.push(make_pair(value, key));
-        }
+        // convert hashMap to array and sort
+        vector<pair<int, int>> hashArray(mpp.begin(), mpp.end());
+        sort(hashArray.begin(), hashArray.end(), comparator);
+
         // returing the top k elements
         vector<int> result;
         for(int i=0;i<k;i++){
-            result.push_back(pq.top().second);
-            pq.pop();
+            result.push_back(hashArray[i].first);
         }
         return result;
     }
