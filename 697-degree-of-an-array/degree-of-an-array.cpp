5593f6f048c2ab33260ebc6cabd5aebd2136a279
Kulpreet Singh <43897531+Kulpreet-Singh@users.noreply.github.com>
2024-05-17 03:45:45 +0530

    Time: 30 ms (56.22%) | Memory: 29.2 MB (41.84%) - LeetSync

--- 697-degree-of-an-array/degree-of-an-array.cpp
+++ 697-degree-of-an-array/degree-of-an-array.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     int findShortestSubArray(vector<int>& nums) {
-        unordered_map<int, vector<int>> mpp;
+        unordered_map<int, pair<int, int>> mpp;
+        unordered_map<int, int> freq;
+
         int n = nums.size();
         int maxFreq = 0;
         for(int i=0;i<n;i++){
             int num = nums[i];
+            freq[num]++;
             if(mpp.find(num)==mpp.end()){
-                mpp[num] = {};
+                mpp[num] = make_pair(i, i);
             }
-            mpp[num].push_back(i);
-            if(mpp[num].size()>maxFreq){
-                maxFreq = mpp[num].size();
+            mpp[num].second = i;
+            if(freq[num]>maxFreq){
+                maxFreq = freq[num];
             }
         }
         int minLen = INT_MAX;
-        for(auto it: mpp){
-            int size = it.second.size();
+        for(auto it: freq){
+            int size = it.second;
             if(size==maxFreq){
-                int len = it.second.back() - it.second.front() + 1;
+                int len = mpp[it.first].second - mpp[it.first].first + 1;
                 if(len<minLen){
                     minLen = len;
                 }
