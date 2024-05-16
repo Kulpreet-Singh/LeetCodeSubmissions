02f9a6e5d6fceb46fd2e9c2c245b031917368a34
Kulpreet Singh <43897531+Kulpreet-Singh@users.noreply.github.com>
2024-05-17 03:54:47 +0530

    Time: 21 ms (91.12%) | Memory: 29.2 MB (47.65%) - LeetSync

--- 697-degree-of-an-array/degree-of-an-array.cpp
+++ 697-degree-of-an-array/degree-of-an-array.cpp
@@ -6,21 +6,26 @@ public:
 
         int n = nums.size();
         int maxFreq = 0;
+        // freq hash
         for(int i=0;i<n;i++){
-            int num = nums[i];
-            freq[num]++;
-            if(mpp.find(num)==mpp.end()){
-                mpp[num] = make_pair(i, i);
-            }
-            mpp[num].second = i;
-            if(freq[num]>maxFreq){
-                maxFreq = freq[num];
+            freq[nums[i]]++;
+            if(freq[nums[i]]>maxFreq){
+                maxFreq = freq[nums[i]];
             }
         }
+        // if max freq, then maintain first and last index
+        for(int i=0;i<n;i++){
+            if(freq[nums[i]] == maxFreq){
+                if(mpp.find(nums[i])==mpp.end()){
+                    mpp[nums[i]] = make_pair(i, i);
+                }
+                mpp[nums[i]].second = i;
+            }
+        }
+        // if maxFreq, then calculate minLen
         int minLen = INT_MAX;
         for(auto it: freq){
-            int size = it.second;
-            if(size==maxFreq){
+            if(it.second==maxFreq){
                 int len = mpp[it.first].second - mpp[it.first].first + 1;
                 if(len<minLen){
                     minLen = len;
