c7315d50a7f2cc2259df87259462e040107b4c2f
Kulpreet Singh <43897531+Kulpreet-Singh@users.noreply.github.com>
2024-05-18 00:27:52 +0530

    Time: 146 ms (48.30%) | Memory: 126.2 MB (84.67%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,44 +1,18 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> result;
-        int p = 0, n = 0;
-        int bit = 0;
-        while (p < nums.size() && n < nums.size()) {
-            if (bit == 0) {
-                if (nums[p] > 0) {
-                    result.push_back(nums[p++]);
-                    bit = 1;
-                } else {
-                    p++;
-                }
+        int p=0, n=1;
+        vector<int> result(nums.size(), 0);
+        for(auto num: nums){
+            if(num>0){
+                result[p] = num;
+                p+=2;
             } else {
-                if (nums[n] < 0) {
-                    result.push_back(nums[n++]);
-                    bit = 0;
-                } else {
-                    n++;
-                }
+                result[n] = num;
+                n+=2;
             }
         }
-        while (p < nums.size() && bit == 0) {
-                if (nums[p] > 0) {
-                    result.push_back(nums[p++]);
-                    bit = 1;
-                } else {
-                    p++;
-                }
-            
-        }
-        while (n < nums.size() && bit == 1) {
-                if (nums[n] < 0) {
-                    result.push_back(nums[n++]);
-                    bit = 0;
-                } else {
-                    n++;
-                }
-            
-        }
+        
         return result;
     }
 };
