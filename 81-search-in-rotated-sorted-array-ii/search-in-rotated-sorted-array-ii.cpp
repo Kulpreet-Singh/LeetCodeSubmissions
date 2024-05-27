6a72a78441ed22d284c46183428e94d56d7b036a
Kulpreet Singh <43897531+Kulpreet-Singh@users.noreply.github.com>
2024-05-27 21:35:34 +0530

    Time: 3 ms (80.63%) | Memory: 16.6 MB (28.03%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -12,13 +12,13 @@ public:
                 low++;
                 high--;
             } else if(nums[low]<=nums[mid]){
-                if(target>=nums[low] && target<=nums[mid])
+                if(target>=nums[low] && target<nums[mid])
                     high = mid-1;
                 else 
                     low = mid+1;
             }
             else {
-                if(target>=nums[mid] && target<=nums[high])
+                if(target>nums[mid] && target<=nums[high])
                     low = mid+1;
                 else 
                     high = mid-1;
