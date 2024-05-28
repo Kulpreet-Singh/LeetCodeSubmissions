efb37233c559449d766310a266076f5a75ee9ac2
Kulpreet Singh <43897531+Kulpreet-Singh@users.noreply.github.com>
2024-05-28 23:23:34 +0530

    Time: 15 ms (85.37%) | Memory: 24.5 MB (85.28%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -11,14 +11,16 @@ public:
         while (low <= high) {
             int mid = low + (high - low) / 2;
             if (mid % 2 == 0) {
-                if (((mid == 0) || (nums[mid] != nums[mid - 1])) &&
-                    ((mid == n - 1) || (nums[mid] != nums[mid + 1]))) {
-                    return nums[mid];
-                } else if (nums[mid] == nums[mid - 1]) {
-                    // move to left half
-                    high = mid - 1;
-                } else {
+                if (mid!=n-1 && nums[mid] == nums[mid + 1]) {
+                    // move to right half
                     low = mid + 1;
+                } else {
+                    if (mid!=0 && nums[mid] == nums[mid - 1]) {
+                        // move to left half
+                        high = mid - 1;
+                    } else {
+                        return nums[mid];
+                    }
                 }
 
             } else {
