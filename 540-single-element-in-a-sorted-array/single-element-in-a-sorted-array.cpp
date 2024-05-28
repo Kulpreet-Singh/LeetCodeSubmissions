53e67463dcc9e12920771bf4fa8ca963897d0b3a
Kulpreet Singh <43897531+Kulpreet-Singh@users.noreply.github.com>
2024-05-28 23:15:55 +0530

    Time: 22 ms (31.37%) | Memory: 24.6 MB (85.28%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,11 +1,35 @@
 class Solution {
 public:
-// O(n) approach using xor
+    // O(n) approach using xor
     int singleNonDuplicate(vector<int>& nums) {
-        int val = 0;
-        for(auto num: nums){
-            val = val ^ num;
+        int n = nums.size();
+        if (n == 1)
+            return nums[0];
+
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (mid % 2 == 0) {
+                if (((mid == 0) || (nums[mid] != nums[mid - 1])) &&
+                    ((mid == n - 1) || (nums[mid] != nums[mid + 1]))) {
+                    return nums[mid];
+                } else if (nums[mid] == nums[mid - 1]) {
+                    // move to left half
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+
+            } else {
+                if (nums[mid] == nums[mid + 1]) {
+                    // move to left half
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
         }
-        return val;
+        return -1;
     }
 };
