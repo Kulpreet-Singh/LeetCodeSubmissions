b969daddbae74332f5cb546d7fe3f4dcd1e765ff
Kulpreet Singh <43897531+Kulpreet-Singh@users.noreply.github.com>
2024-06-01 21:22:11 +0530

    Time: 5 ms (20.62%) | Memory: 7.3 MB (7.22%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -3,15 +3,11 @@ public:
     bool isPowerOfTwo(int n) {
         if(n<=0) return false;
         int count = 0;
-        int powOf2 = 1;
         for(int i=0;i<31; i++){
-            powOf2 = 1<<i;
+            int powOf2 = 1<<i;
             if((n & powOf2) == powOf2){
                 count++;
             } 
-        }
-        if(abs(n) > abs(powOf2)){
-            return false;
         }
         if(count==1){
             return true;
