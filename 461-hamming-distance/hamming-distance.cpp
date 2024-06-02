3c9e2c553af9fadf60113f1929f92826cbb538e9
Kulpreet Singh <43897531+Kulpreet-Singh@users.noreply.github.com>
2024-06-02 13:12:02 +0530

    Time: 3 ms (40.47%) | Memory: 7.2 MB (54.87%) - LeetSync

--- 461-hamming-distance/hamming-distance.cpp
+++ 461-hamming-distance/hamming-distance.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     int hammingDistance(int x, int y) {
         int count = 0;
-        for (int i = 0; i < 31; i++) {
-            int mask = 1 << i;
-            if (((x & mask) == mask && (y & mask) == 0) ||
-                ((x & mask) == 0 && (y & mask) == mask)) {
+        while (x > 0 || y > 0) {
+            if (((x & 1) == 1 && (y & 1) == 0) ||
+                ((x & 1) == 0 && (y & 1) == 1)) {
                 count++;
             }
+            x = x >> 1;
+            y = y >> 1;
         }
         return count;
     }
