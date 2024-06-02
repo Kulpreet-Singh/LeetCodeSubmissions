1669259636a2425407c19f4f87e27e1b9d1bc489
Kulpreet Singh <43897531+Kulpreet-Singh@users.noreply.github.com>
2024-06-02 13:14:32 +0530

    Time: 3 ms (40.47%) | Memory: 7.2 MB (16.68%) - LeetSync

--- 461-hamming-distance/hamming-distance.cpp
+++ 461-hamming-distance/hamming-distance.cpp
@@ -3,8 +3,7 @@ public:
     int hammingDistance(int x, int y) {
         int count = 0;
         while (x > 0 || y > 0) {
-            if (((x & 1) == 1 && (y & 1) == 0) ||
-                ((x & 1) == 0 && (y & 1) == 1)) {
+            if ((x & 1) != (y & 1)) {
                 count++;
             }
             x = x >> 1;
