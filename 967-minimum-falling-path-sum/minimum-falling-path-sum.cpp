a9f6d6cd50d0915382bdc5697324d5806367e90e
Kulpreet Singh <43897531+Kulpreet-Singh@users.noreply.github.com>
2024-06-10 22:56:56 +0530

    Time: 12 ms (58.95%) | Memory: 13.6 MB (25.16%) - LeetSync

---
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    // int helper(int i, int j, int& n, vector<vector<int>>& matrix,
+    //            vector<vector<int>>& dp) {
+    //     if (j < 0 || j >= n)
+    //         return 1e9;
+    //     if (dp[i][j] != -1)
+    //         return dp[i][j];
+    //     if (i == 0)
+    //         return dp[i][j] = matrix[i][j];
+        
+    //     return dp[i][j] =
+    //                matrix[i][j] + min(min(helper(i - 1, j - 1, n, matrix, dp),
+    //                                       helper(i - 1, j, n, matrix, dp)),
+    //                                   helper(i - 1, j + 1, n, matrix, dp));
+    // }
+    // int minFallingPathSum(vector<vector<int>>& matrix) {
+    //     int n = matrix.size();
+    //     int ans = 1e9;
+    //     vector<vector<int>> dp(n, vector<int>(n, -1));
+    //     for (int i = 0; i < n; i++) {
+    //         ans = min(ans, helper(n - 1, i, n, matrix, dp));
+    //     }
+    //     return ans;
+    // }
+
+    // tabulation
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        for(int i=0;i<n;i++){
+            dp[0][i] = matrix[0][i];
+        }
+        for(int i=1;i<n;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j] = matrix[i][j] + dp[i-1][j];
+                if(j>0){
+                    dp[i][j] = min(dp[i][j],  matrix[i][j] + dp[i-1][j-1]);
+                }
+                if(j<n-1){
+                    dp[i][j] = min(dp[i][j],  matrix[i][j] + dp[i-1][j+1]);
+                }
+            }
+        }
+        int ans = 1e9;
+        for(int i=0;i<n;i++){
+            ans = min(ans, dp[n-1][i]);
+        }
+        return ans;
+    }
+    
+};
