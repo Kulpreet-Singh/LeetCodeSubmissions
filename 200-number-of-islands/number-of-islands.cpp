ed38c93a52b942fd94464c052c242a398d8c06e9
Kulpreet Singh <43897531+Kulpreet-Singh@users.noreply.github.com>
2024-07-10 01:04:25 +0530

    Time: 38 ms (19.37%) | Memory: 23.5 MB (23.15%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -11,6 +11,29 @@ public:
         dfs(i, j+1, m, n, grid, visited);
     }
 
+    void bfs(int i, int j,int m, int n, vector<vector<char>>& grid, vector<vector<bool>>& visited){
+        vector<pair<int, int>> directions = {{1,0}, {-1, 0}, {0, 1}, {0, -1}};
+        queue<pair<int, int>> q;
+        q.push({i, j});
+        while(!q.empty()){
+            auto [x, y] = q.front();
+            q.pop();
+
+            if(x== -1 || y == -1 || x==m || y ==n || grid[x][y]=='0' || visited[x][y]){
+                continue;
+            }
+            visited[x][y] = true;
+            for(auto& [dx, dy]: directions){
+                int nx = x + dx;
+                int ny = y+ dy;
+                if(nx>=0 && nx<m && ny>=0 && ny<n && grid[nx][ny]=='1' && !visited[nx][ny]){
+                    q.push({nx, ny});
+                }
+            }
+        }
+
+    }
+
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
@@ -19,7 +42,7 @@ public:
         for(int i=0;i<m; i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1' && !visited[i][j]){
-                    dfs(i, j,m,n, grid, visited);
+                    bfs(i, j,m,n, grid, visited);
                     count++;
                 }
             }
