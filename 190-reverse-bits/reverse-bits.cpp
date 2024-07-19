884e722a473bf1c0699c92716dc168fac017116b
Kulpreet Singh <43897531+Kulpreet-Singh@users.noreply.github.com>
2024-07-20 03:17:35 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (13.53%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        uint32_t output = 0;
-        for(int i=0;i<32;i++){
-            if(n&(1<<i)){
-                output = output|(1<<(31-i));
-            }
+        uint32_t ans = 0;
+        for (int i = 0; i < 32; i++) {
+            ans <<= 1;
+            ans |= (n & 1);
+            n >>= 1;
         }
-        return output;
+        return ans;
     }
 };
