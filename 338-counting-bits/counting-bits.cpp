83445e45317a48c31e6f8081745ea398c4dee88b
Kulpreet Singh <43897531+Kulpreet-Singh@users.noreply.github.com>
2024-07-20 03:36:05 +0530

    Time: 5 ms (49.76%) | Memory: 9.2 MB (46.91%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         vector<int> dp(n + 1, 0);
-        dp[0] = 0;
         for (int i = 1; i <= n; i++) {
             dp[i] = dp[i / 2] + (i % 2);
         }
