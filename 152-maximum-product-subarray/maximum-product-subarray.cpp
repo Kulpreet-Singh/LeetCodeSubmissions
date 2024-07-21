f5a46413231fced1a2eff83151ef7511c2d679f4
Kulpreet Singh <43897531+Kulpreet-Singh@users.noreply.github.com>
2024-07-21 14:34:44 +0530

    Time: 3 ms (84.82%) | Memory: 16.6 MB (11.75%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,32 +1,16 @@
 class Solution {
 public:
-    int maxProduct(vector<int>& nums) {
-        if (nums.size() == 1)
-            return nums[0];
-        int maxProduct = INT_MIN;
-        double currMinProduct = 1;
-        double currMaxProduct = 1;
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == 0) {
-                currMinProduct = 0;
-                currMaxProduct = 0;
-            } else if (nums[i] > 0) {
-                currMaxProduct *= nums[i];
-                currMinProduct = min((double)0, currMinProduct * nums[i]);
-            } else {
-                double temp = currMaxProduct;
-                currMaxProduct = max((double)0, currMinProduct * nums[i]);
-                currMinProduct = temp * nums[i];
-            }
-            if (maxProduct < currMaxProduct) {
-                maxProduct = currMaxProduct;
-            }
-            if (currMaxProduct == 0)
-                currMaxProduct = 1;
-            if (currMinProduct == 0)
-                currMinProduct = 1;
-        }
-        return maxProduct;
+  int maxProduct(vector<int>& A) {
+    int n = A.size();
+    double ans = INT_MIN;
+    double pr = 1, su = 1;
+    for (int i=0; i<n; ++i) {
+      pr *= A[i];
+      su *= A[n-1-i];
+      ans = max({ans, pr, su});
+      if (pr == 0) pr = 1;
+      if (su == 0) su = 1;
     }
+    return ans;
+  }
 };
